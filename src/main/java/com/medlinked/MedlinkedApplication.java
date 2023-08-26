8fcd0c72971c1013ef1bcff546efa6088fc4c0ee
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-08-26 02:06:16 -0300

    feat: iniciando projeto e fazendo configurações para service registry

---
+++ src/main/java/com/medlinked/MedlinkedApplication.java
@@ -0,0 +1,13 @@
+package com.medlinked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlinkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlinkedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medlinked/MedlinkedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medlinked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlinkedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
