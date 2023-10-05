a23759bb373a851cfe61f021eb37715961ff4aac
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-10-05 18:39:32 -0300

    feat: validando crm

--- src/main/java/com/medlinked/services/impl/MedicoCrmServiceImpl.java
+++ src/main/java/com/medlinked/services/impl/MedicoCrmServiceImpl.java
@@ -5,6 +5,7 @@ import com.medlinked.entities.Especialidade;
 import com.medlinked.entities.Medico;
 import com.medlinked.entities.dtos.MedicoDto;
 import com.medlinked.exceptions.EspecialidadeException;
+import com.medlinked.exceptions.ExistsException;
 import com.medlinked.repositories.MedicoCrmRepository;
 import com.medlinked.services.EstadoService;
 import com.medlinked.services.MedicoCrmService;
@@ -33,8 +34,7 @@ public class MedicoCrmServiceImpl implements MedicoCrmService {
     @Override
     @Transactional
     public MedicoCRM createCrmMedico(Medico medico, MedicoDto medicoDto) {
-        if(BooleanUtils.isTrue(medicoDto.getIdsEspecialidades().size() > 2))
-            throw new EspecialidadeException();
+        this.validateCrm(medicoDto);
         MedicoCRM medicoCrm = MedicoCRM
                 .builder()
                 .medico(medico)
@@ -55,11 +55,20 @@ public class MedicoCrmServiceImpl implements MedicoCrmService {
         return medicoCrmRepository.getEspecialidadesMedicoByCrm(idMedico);
     }
 
+    private void validateCrm(MedicoDto medicoDto) {
+        if(BooleanUtils.isTrue(medicoDto.getIdsEspecialidades().size() > 2))
+            throw new EspecialidadeException();
+        if(this.existsMedicoByNumeroCrm(medicoDto.getNumeroCrm()))
+            throw new ExistsException("Médico", "Número CRM");
+    }
+    private boolean existsMedicoByNumeroCrm(Integer idMedico) {
+        return medicoCrmRepository.existsMedicoByNumeroCrm(idMedico);
+    }
+
     @Override
     @Transactional
     public MedicoCRM updateMedicoCrm(Medico medico, MedicoDto medicoDto) {
-        if(BooleanUtils.isTrue(medicoDto.getIdsEspecialidades().size() > 2))
-            throw new EspecialidadeException();
+        this.validateCrm(medicoDto);
         MedicoCRM medicoCRM = medicoCrmRepository.getOneCrmByMedico(medico.getIdMedico());
         medicoCRM.setMedico(medico);
         medicoCRM.setNumeroCrm(medicoDto.getNumeroCrm());
