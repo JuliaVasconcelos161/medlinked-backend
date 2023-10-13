f750aef0e14f9f5c6fd1af395be43a78c173038d
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-10-12 23:53:30 -0300

    feat: cria método que retorna secretária baseado no idSecretaria informado

--- src/main/java/com/medlinked/controllers/SecretariaController.java
+++ src/main/java/com/medlinked/controllers/SecretariaController.java
@@ -1,7 +1,7 @@
 package com.medlinked.controllers;
 
+import com.medlinked.entities.dtos.SecretariaUsuarioDto;
 import com.medlinked.entities.dtos.SecretariaDto;
-import com.medlinked.entities.dtos.SecretariaUpdateDto;
 import com.medlinked.exceptions.MedLinkedException;
 import com.medlinked.services.secretaria_service.SecretariaService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,9 +23,9 @@ public class SecretariaController {
 
     @Operation(summary = "Cria secretária e retorna jwt de autenticacao.")
     @PostMapping("/create")
-    public ResponseEntity<Object> createSecretaria(@RequestBody @Valid SecretariaDto secretariaDto) {
+    public ResponseEntity<Object> createSecretaria(@RequestBody @Valid SecretariaUsuarioDto secretariaUsuarioDto) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(secretariaService.createSecretaria(secretariaDto));
+            return ResponseEntity.status(HttpStatus.OK).body(secretariaService.createSecretaria(secretariaUsuarioDto));
         }catch (MedLinkedException e) {
             return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
         }
@@ -34,9 +34,19 @@ public class SecretariaController {
     @Operation(summary = "Atualiza secretária e retorna secretaria com dados atualizados.")
     @PutMapping("/update/{idSecretaria}")
     public ResponseEntity<Object> updateSecretaria(@PathVariable Integer idSecretaria,
-                                                   @RequestBody @Valid SecretariaUpdateDto secretariaUpdateDto) {
+                                                   @RequestBody @Valid SecretariaDto secretariaDto) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(secretariaService.updateSecretaria(secretariaUpdateDto, idSecretaria));
+            return ResponseEntity.status(HttpStatus.OK).body(secretariaService.updateSecretaria(secretariaDto, idSecretaria));
+        }catch (MedLinkedException e) {
+            return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+        }
+    }
+
+    @Operation(summary = "Retorna uma secretária utilizando o idSecretaria informado.")
+    @GetMapping("/{idSecretaria}")
+    public ResponseEntity<Object> getOneSecretaria(@PathVariable Integer idSecretaria) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(secretariaService.getOneSecretaria(idSecretaria));
         }catch (MedLinkedException e) {
             return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
         }
--- src/main/java/com/medlinked/entities/dtos/SecretariaDto.java
+++ src/main/java/com/medlinked/entities/dtos/SecretariaDto.java
@@ -1,17 +1,10 @@
 package com.medlinked.entities.dtos;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class SecretariaDto extends PessoaDto {
-
-    @NotNull
-    private UsuarioRegisterDto usuarioRegisterDto;
-
-
 }
