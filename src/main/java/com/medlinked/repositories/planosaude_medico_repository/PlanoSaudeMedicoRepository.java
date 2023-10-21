251243a9aa0e00ac0cda4cf37b9ee6caf98fc3af
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-10-21 16:55:45 -0300

    refactor: remove método não utilizado

--- src/main/java/com/medlinked/repositories/planosaude_medico_repository/PlanoSaudeMedicoRepository.java
+++ src/main/java/com/medlinked/repositories/planosaude_medico_repository/PlanoSaudeMedicoRepository.java
@@ -1,12 +1,10 @@
 package com.medlinked.repositories.planosaude_medico_repository;
 
-import com.medlinked.entities.Medico;
 import com.medlinked.entities.PlanoSaude;
 
 import java.util.List;
 
 public interface PlanoSaudeMedicoRepository {
-    List<Medico> getAllMedicosPlanoSaude(Integer idPlanoSaude);
 
     List<PlanoSaude> getAllPlanosSaudeMedico(Integer idMedico, int page, int pageSize);
 
--- src/main/java/com/medlinked/repositories/planosaude_medico_repository/PlanoSaudeMedicoRepositoryClass.java
+++ src/main/java/com/medlinked/repositories/planosaude_medico_repository/PlanoSaudeMedicoRepositoryClass.java
@@ -1,6 +1,5 @@
 package com.medlinked.repositories.planosaude_medico_repository;
 
-import com.medlinked.entities.Medico;
 import com.medlinked.entities.PlanoSaude;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -13,17 +12,6 @@ public class PlanoSaudeMedicoRepositoryClass implements PlanoSaudeMedicoReposito
 
     @PersistenceContext
     EntityManager entityManager;
-
-    @Override
-    public List<Medico> getAllMedicosPlanoSaude(Integer idPlanoSaude) {
-        StringBuilder consulta = new StringBuilder(" select medico ");
-        consulta.append(" from Medico medico ");
-        consulta.append(" inner join medico.planosSaude planoSaude ");
-        consulta.append(" where planoSaude.idPlanoSaude = :IDPLANOSAUDE ");
-        var query = entityManager.createQuery(consulta.toString(), Medico.class);
-        query.setParameter("IDPLANOSAUDE", idPlanoSaude);
-        return query.getResultList();
-    }
 
     @Override
     public List<PlanoSaude> getAllPlanosSaudeMedico(Integer idMedico, int page, int pageSize) {
